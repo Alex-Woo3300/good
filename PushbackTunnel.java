54ce679b1def666adbb727f1dce54dd885151d74
Alex-Woo3300 <ADW148@pitt.edu>
2026-09-24 22:09:25 -0400

    Add files via upload

---
+++ PushbackTunnel.java
@@ -0,0 +1,10 @@
+public class PushbackTunnel implements PushbackTunnelInterface {
+
+    public PushbackTunnel() {
+        // TODO: fill in based on autograder feedback
+    }
+
+    // TODO: add methods required by PushbackTunnelInterface.
+    // The compiler will list any missing overrides once this file
+    // is in place alongside PushbackTunnelInterface.java.
+}
