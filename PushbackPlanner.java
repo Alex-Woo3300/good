520499b3d48a0a2449fa12dd1b320cbf16a94ddd
Alex-Woo3300 <ADW148@pitt.edu>
2026-09-24 22:09:05 -0400

    Add files via upload

---
+++ PushbackPlanner.java
@@ -0,0 +1,8 @@
+public class PushbackPlanner {
+
+    // TODO: figure out real parameter types/order from BoggleGameTest.java calls
+    // and from the compiler errors that come back once this stub is in place.
+    public static Choice[] findWinningSequence(Object... args) {
+        return new Choice[0];
+    }
+}
